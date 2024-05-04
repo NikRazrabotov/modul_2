d779b0d0a5b51e09d515dba8e38bc82757ee9bc6
Viktor Didov <razrabotov@yandex.ru>
2024-05-04 13:24:40 +0200

    Закончил_модуль_2_Разобраться_С_отправкой_ДЗ

---
+++ modul_2/А_Р_Модуль 2.mm
@@ -0,0 +1,152 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Андроид разработка" FOLDED="false" ID="ID_1090958577" CREATED="1606664858024" MODIFIED="1714814571197">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#3c3836" zoom="0.77">
+    <conditional_styles>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="default" LAST="false"/>
+    </conditional_styles>
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/dark_gruvbox_template.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_602083445" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#3c3836" BACKGROUND_COLOR="#33ffcc" STYLE="rectangle" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#886ee8" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#a89984" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_602083445" STARTINCLINATION="102.75 pt;-10.5 pt;" ENDINCLINATION="102.75 pt;3 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#458588" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#458588"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_749235638" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#cc241d">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#cc241d" TRANSPARENCY="255" DESTINATION="ID_749235638"/>
+<font SIZE="12" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#fdf6e3" BACKGROUND_COLOR="#282828" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true" VGAP_QUANTITY="3 pt">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#282828" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#282828" BACKGROUND_COLOR="#ebdbb2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#282828" BACKGROUND_COLOR="#d79921" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffffff" BACKGROUND_COLOR="#98971a" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#ffffff" BACKGROUND_COLOR="#b16286" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#ffffff" BACKGROUND_COLOR="#689d6a" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#ffffff" BACKGROUND_COLOR="#a89984" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#ebdbb2" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0" BACKGROUND_COLOR="#ebdbb2">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="NodeConditionalStyles">
+    <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="default" LAST="false"/>
+</hook>
+<node TEXT="Модуль 2" POSITION="bottom_or_right" ID="ID_1562238961" CREATED="1714814609769" MODIFIED="1714814614454">
+<node TEXT="2.3 Анализ проекта" FOLDED="true" ID="ID_285364464" CREATED="1714814707797" MODIFIED="1714814718249">
+<node TEXT="Мои Заметки" ID="ID_1130668242" CREATED="1714815246571" MODIFIED="1714815551374"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <div style="background-color: #1e1f22; color: #bcbec4">
+      <pre style="font-family: JetBrains Mono,monospace; font-size: 9,8pt"><span style="color: #cf8e6d;">package </span>org.example<br/><br style="color: #7a7e85;"/><span style="color: #7a7e85;">To Run code, press &#xa0;Ctrl&#xa0;&#xa0;&#xa0;F5&#xa0; or click the   icon in the gutter</span><br style="color: #7a7e85;"/><span style="color: #cf8e6d;">fun </span><span style="color: #56a8f5;">main</span>() {<br/>    <span style="color: #cf8e6d;">val </span>name = <span style="color: #6aab73;">&quot;Kotlin&quot;</span><br style="color: #6aab73;"/><span style="color: #6aab73;">   
+Press &#xa0;Ctrl&#xa0;&#xa0;&#xa0;Period&#xa0; with your caret at the highlighted text to see how IntelliJ IDEA suggests fixing it.
+
+Press &#xa0;F5&#xa0; to start debugging your code. We have set one   breakpoint for you, but you can always add more by pressing &#xa0;F9&#xa0;.
+
+</span><br style="color: #7a7e85;"/><span style="color: #7a7e85;">    </span><span style="font-style: italic;">println</span>(<span style="color: #6aab73;">&quot;Hello, &quot; </span>+ name + <span style="color: #6aab73;">&quot;!&quot;</span>)<br/>
+<br/>    <span style="color: #cf8e6d;">for </span>(i <span style="color: #cf8e6d;">in </span><span style="color: #2aacb8;">1</span>..<span style="color: #2aacb8;">5</span>) {<br/>       <span style="color: #7a7e85;"> </span><span style="font-style: italic;">println</span>(<span style="color: #6aab73;">&quot;i = </span><span style="color: #cf8e6d;">$</span>i<span style="color: #6aab73;">&quot;</span>)<br/>    }<br/>}</pre>
+    </div>
+  </body>
+</html>
+</richcontent>
+<node TEXT="структура проекта" ID="ID_1697090353" CREATED="1714816021368" MODIFIED="1714816027681">
+<node TEXT="Структура_проекта_1.png" ID="ID_1590633098" CREATED="1714816144888" MODIFIED="1714816144888">
+<hook URI="А_Р_Модуль%202_files/Структура_проекта_1.png" SIZE="1.0" NAME="ExternalObject"/>
+<node TEXT="Настройки и скрипты проекта" ID="ID_1855502390" CREATED="1714816158789" MODIFIED="1714816168807"/>
+</node>
+<node TEXT="Папка .idea" ID="ID_1274999965" CREATED="1714816185574" MODIFIED="1714816194177">
+<node TEXT="Это папка где хранятся конфиги среды разработки&#xa;Локальная папка" ID="ID_304456493" CREATED="1714816194440" MODIFIED="1714816225063"/>
+</node>
+<node TEXT="Больше всего интересует папка src" ID="ID_1049992582" CREATED="1714816276032" MODIFIED="1714816524166" BACKGROUND_COLOR="#ff66ff">
+<node TEXT="main" ID="ID_1852766681" CREATED="1714816304806" MODIFIED="1714816514376" BACKGROUND_COLOR="#ff66ff">
+<node TEXT="Будет размещаться весь написанный код" ID="ID_696405997" CREATED="1714816312139" MODIFIED="1714816322104"/>
+<node TEXT=".kotlin" ID="ID_1842793355" CREATED="1714816364057" MODIFIED="1714816514376" BACKGROUND_COLOR="#ff66ff">
+<node TEXT="Размещает в себе kotlin файлы&#xa;в котором будет написан код проги" ID="ID_144979689" CREATED="1714816376955" MODIFIED="1714816412298"/>
+<node TEXT="main.kt" ID="ID_962537481" CREATED="1714816471532" MODIFIED="1714816514376" BACKGROUND_COLOR="#ff66ff">
+<node TEXT="В нем пишем код" ID="ID_550905001" CREATED="1714816485602" MODIFIED="1714816501913" BACKGROUND_COLOR="#ff66ff"/>
+</node>
+</node>
+<node TEXT=".java" ID="ID_733270742" CREATED="1714816422881" MODIFIED="1714816427508">
+<node TEXT="Содержит тоже самое что и котлин, только java" ID="ID_1171559254" CREATED="1714816427962" MODIFIED="1714816441232"/>
+</node>
+</node>
+<node TEXT="test" ID="ID_1209783617" CREATED="1714816307898" MODIFIED="1714816309950">
+<node TEXT="Размещаются тесты которые проверяют работоспособность кода" ID="ID_1368145205" CREATED="1714816333048" MODIFIED="1714816348965"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="2.4 Функции main, println, Коменты.&#xa;Первая прога" ID="ID_711724767" CREATED="1714816562523" MODIFIED="1714816590468">
+<node TEXT="Внутри main.kt" ID="ID_928382069" CREATED="1714816653752" MODIFIED="1714816667463">
+<node TEXT="Первая строка, первая функция главная" ID="ID_586561348" CREATED="1714816667842" MODIFIED="1714816687543">
+<node TEXT="fun main() {&#xa;        println(&quot;Hello World&quot;)&#xa;}" ID="ID_796229350" CREATED="1714816687981" MODIFIED="1714816722413">
+<node TEXT="println= это еще одна функция= - выводит то что в скобках в консоль, и переводит курсор на следующию строку.&#xa;то есть если 2 подрят println то будет&#xa;Hello World&#xa;Hello World" ID="ID_617382711" CREATED="1714816752999" MODIFIED="1714817071652"/>
+</node>
+<node TEXT="print(&quot;hello&quot;)" ID="ID_1058964214" CREATED="1714817075264" MODIFIED="1714817093483">
+<node TEXT="Выводит в консоль, оставляет курсор на тойже строке" ID="ID_507693288" CREATED="1714817093825" MODIFIED="1714817110164"/>
+</node>
+<node TEXT="Коменты" ID="ID_1891791639" CREATED="1714817148956" MODIFIED="1714817153095">
+<node TEXT="Закоментировать строку //" ID="ID_1796986513" CREATED="1714817154413" MODIFIED="1714817165068">
+<node TEXT="Комбинация клавиш ctrl + /" ID="ID_356171412" CREATED="1714817165301" MODIFIED="1714817180017"/>
+</node>
+<node TEXT="/*Закоментировать несколько строк*/" ID="ID_48877796" CREATED="1714817186607" MODIFIED="1714817219444">
+<node TEXT="Ком кл ctrl + shift + /" ID="ID_1506988315" CREATED="1714817220073" MODIFIED="1714817239837"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
